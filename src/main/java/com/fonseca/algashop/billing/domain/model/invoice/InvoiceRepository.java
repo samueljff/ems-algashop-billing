f0ba9c11b858fdc8a31cca66ffc46a2327731d63
samueljff <samueljosef3@gmail.com>
2026-02-20 23:32:08 -0300

    Repository orientado a persistência

---
+++ src/main/java/com/fonseca/algashop/billing/domain/model/invoice/InvoiceRepository.java
@@ -0,0 +1,8 @@
+package com.fonseca.algashop.billing.domain.model.invoice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {
+}
