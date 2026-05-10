5d0d632e2afa59bc1e680ee5570be462510774d7
samueljff <samueljosef3@gmail.com>
2026-05-10 16:40:51 -0300

    Aprimorando respostas da API de Customer

--- src/main/java/com/fonseca/algashop/billing/BillingApplication.java
+++ src/main/java/com/fonseca/algashop/billing/BillingApplication.java
@@ -3,11 +3,15 @@ package com.fonseca.algashop.billing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BillingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BillingApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(BillingApplication.class, args);
+    }
 
 }
