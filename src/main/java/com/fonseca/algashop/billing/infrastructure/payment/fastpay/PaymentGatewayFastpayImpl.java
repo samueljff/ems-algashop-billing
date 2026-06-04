0e0e6eb2656f0289dc38bd4f02f7759d95868e17
samueljff <samueljosef3@gmail.com>
2026-06-03 22:07:13 -0300

    Domain Service de pagamentos como ACL

--- src/main/java/com/fonseca/algashop/billing/infrastructure/payment/fastpay/PaymentGatewayFastpayImpl.java
+++ src/main/java/com/fonseca/algashop/billing/infrastructure/payment/fastpay/PaymentGatewayFastpayImpl.java
@@ -1,22 +1,92 @@
 package com.fonseca.algashop.billing.infrastructure.payment.fastpay;
 
+import com.fonseca.algashop.billing.domain.model.creditcard.CreditCard;
+import com.fonseca.algashop.billing.domain.model.creditcard.CreditCardNotFoundException;
+import com.fonseca.algashop.billing.domain.model.creditcard.CreditCardRepository;
+import com.fonseca.algashop.billing.domain.model.invoice.Address;
+import com.fonseca.algashop.billing.domain.model.invoice.Payer;
 import com.fonseca.algashop.billing.domain.model.invoice.payment.Payment;
 import com.fonseca.algashop.billing.domain.model.invoice.payment.PaymentGatewayService;
 import com.fonseca.algashop.billing.domain.model.invoice.payment.PaymentRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @ConditionalOnProperty(name = "algashop.integrations.payment.provider", havingValue = "FASTPAY")
+@RequiredArgsConstructor
 public class PaymentGatewayFastpayImpl implements PaymentGatewayService {
+
+    private final FastpayPaymentAPIClient fastpayPaymentAPIClient;
+    private final CreditCardRepository creditCardRepository;
 
     @Override
     public Payment capture(PaymentRequest request) {
-        return null;
+        FastpayPaymentInput input = convertToInput(request);
+        FastpayPaymentModel response = fastpayPaymentAPIClient.capture(input);
+        return convertToPayment(response);
     }
 
     @Override
     public Payment findByCode(String gatewayCode) {
-        return null;
+        FastpayPaymentModel response = fastpayPaymentAPIClient.findById(gatewayCode);
+        return convertToPayment(response);
+    }
+
+    private FastpayPaymentInput convertToInput(PaymentRequest request) {
+        Payer payer = request.getPayer();
+        Address address = payer.getAddress();
+        var builder = FastpayPaymentInput.builder()
+            .totalAmount(request.getAmount())
+            .referenceCode(request.getInvoiceId().toString())
+            .fullName(payer.getFullName())
+            .document(payer.getDocument())
+            .phone(payer.getPhone())
+            .zipCode(address.getZipCode())
+            .addressLine1(address.getStreet() + ", " + address.getNumber())
+            .addressLine2(address.getComplement())
+            .replyToUrl("http://example/webhook");
+
+        switch (request.getPaymentMethod()) {
+            case CREDIT_CARD -> {
+                builder.method(FastpayPaymentMethod.CREDIT.name());
+                CreditCard creditCard = creditCardRepository.findById(request.getCreditCardId())
+                    .orElseThrow(() -> new CreditCardNotFoundException());
+                builder.creditCardId(creditCard.getGatewayCode());
+            }
+            case GATEWAY_BALANCE -> {
+                builder.method(FastpayPaymentMethod.GATEWAY_BALANCE.name());
+            }
+        }
+
+        return builder.build();
+    }
+
+    private Payment convertToPayment(FastpayPaymentModel response) {
+        var builder = Payment.builder()
+            .gateWayCode(response.getId())
+            .invoiceId(UUID.fromString(response.getReferenceCode()));
+
+        FastpayPaymentMethod fastpayPaymentMethod;
+
+        try {
+            fastpayPaymentMethod = FastpayPaymentMethod.valueOf(response.getMethod());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unknown payment method: " + response.getMethod());
+        }
+
+        FastpayPaymentStatus fastpayPaymentStatus;
+        try {
+            fastpayPaymentStatus = FastpayPaymentStatus.valueOf(response.getStatus());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unknown payment status: " + response.getStatus());
+        }
+
+        builder.method(FastpayEnumConverter.convert(fastpayPaymentMethod));
+        builder.status(FastpayEnumConverter.convert(fastpayPaymentStatus));
+
+        return builder.build();
     }
 }
